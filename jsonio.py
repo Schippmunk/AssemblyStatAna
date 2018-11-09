71d8728e9a22dc559cba008420fad030a496e936
Schippmunk <johannes.struve@gmx.de>
2018-11-09 22:42:43 +0000

    - lots of progress in fgets.

---
+++ jsonio.py
@@ -0,0 +1,80 @@
+import json
+from pprint import pprint
+
+inputfilename = ''
+outputfilename = ''
+
+
+def parser(name):
+    """Opens JSON file and calls parse_json with it"""
+
+    global inputfilename
+    inputfilename = "public_tests/test" + name + ".json";
+    global outputfilename
+    outputfilename = "public_tests/test" + name + ".OURoutput.json"
+
+    with open(inputfilename) as f:
+        data = json.load(f)
+
+    create_output_file()
+
+    return parse_json(data)
+
+
+def parse_json(data):
+    """Creates a dictionary of the JSON data and returns it
+
+    keys are function names
+    each value is a dictionary with keys instructions, Ninstructions and variables
+    """
+    result = {}
+
+    # Get function names
+    func_names = data.keys()
+
+    # Parse vars and instrs for each function
+    for f_n in func_names:
+        result[f_n] = {'instructions': data[f_n]['instructions'], 'Ninstructions': data[f_n]['Ninstructions'],
+                       'variables': data[f_n]['variables']}
+
+    for f_n in func_names:
+        result[f_n]['instructions'] = result[f_n]['instructions'][3:]
+
+    # result['main'] = func_instrs['main'][3:]
+    # pprint(result)
+    return result
+
+
+def create_output_file():
+    """Creates the output file with filename outputfilename"""
+    #TODO: actually create the file
+    pass
+
+
+def add_vulnerability(vulnerability):
+    """Appends the vulnerability to filename name
+
+    vulnerability is a dictionary such as
+        "overflown_address": "rbp-0x10",
+        "fnname": "fgets",
+        "vuln_function": "main",
+        "address": "400539",
+        "vulnerability": "INVALIDACCS",
+        "overflow_var": "buf"
+    """
+    print("Adding the following vulnerability to ", outputfilename)
+    pprint(vulnerability)
+    #TODO: actually add it to the file
+
+
+def create_vulnerability(vulnerability='', vuln_function='', fnname='', overflow_var='', address='', overflown_address=''):
+    """Produces a dictionary in the desired output format"""
+    vuln = {
+        'vulnerability': vulnerability,
+        'fnname': fnname,
+        'address': address,
+        'overflow_var': overflow_var,
+        'vuln_function': vuln_function}
+    if not vulnerability == 'RBPOVERFLOW':
+        vuln['overflown_address'] = overflown_address
+    return vuln
--- vuln_checker.py
+++ vuln_checker.py
@@ -1,28 +1,110 @@
-from parser import *
+import jsonio
+
+# pprint used for debugging
+from pprint import pprint
+
+# regular expressions used to match adresses in fgets
+import re
 
 p_data = {}
+register_values = {}
+ourfilename = '';
 
-def check_gets(f_n, instruction):  
-    print(instruction)    
 
-def check_strcpy(f_n, instruction): 
-    print(instruction)
-
-def check_strcat(f_n, instruction): 
-    print(instruction)
-
-def check_fgets(f_n, instruction): 
-    print(f_n)
-    print(instruction)
-    #print(get_var(f_n, 28381))
-
-def check_strncpy(f_n, instruction): 
-    print(instruction)
-
-def check_strncat(f_n, instruction): 
+def check_gets(f_n, instruction):
     print(instruction)
 
 
+def check_strcpy(f_n, instruction):
+    print("\nAnalyzing vulnerability due to fgets in", f_n)
+
+
+def check_strncpy(f_n, instruction):
+    print(instruction)
+
+
+def check_strcat(f_n, instruction):
+    print(instruction)
+
+
+def check_fgets(f_n, instruction):
+    print("\nAnalyzing vulnerability due to fgets in", f_n)
+
+    pprint(p_data)
+
+    # find the second parameter, the length that is read by fgets. It gets moved two positions before the gets call
+    # this call assumes that the parameter is a hardcoded number, not a variable
+    inputlength = get_instruction(f_n, instruction['pos'] - 2)['args']['value']
+    regular_expression = re.compile('0x\d+')
+    if regular_expression.match(inputlength):
+        inputlength = int(inputlength, 0)
+        print("Maximum input length is", inputlength)
+    else:
+        print("ERROR: inputlenght is not a hexadecimal number, but", inputlength)
+        return False
+
+    # find the buffer to copy into
+    # load the instruction
+    bufferinst = get_instruction(f_n, instruction['pos'] - 3);
+    # in the the first 5 tests at least, the buffer is only loaded using lea, and the address depends directly on rbp
+    if bufferinst['op'] == 'lea':
+        buffer_address = bufferinst['args']['value']
+        # see if the address of the buffer saved in that operation is one we support
+        regular_expression = re.compile('\[rbp-0x\d+\]')
+        if regular_expression.match(buffer_address):
+            # cool, buffer_address is of the form [rbp-0x50]
+
+            # clip off [ and ]
+            buffer_address = buffer_address[1:len(buffer_address)-1]
+
+            # find the buffer variable among the local vars of f_n
+            buffer = get_var(f_n, buffer_address)
+            # get the amount of its bytes and convert base
+            bufsize = int(buffer['bytes']);
+
+            print("Size of the buffer is", bufsize)
+
+            # TODO: check if because of nullcharacter at end of string of input, this has to be inputlength < bufsize
+            if inputlength <= bufsize:
+                print("Here is no bufferoverflow possible.")
+                return True
+            else:
+                # now check what can be overflown
+                print("VULNERABILITY: Buffer can be overflown by", inputlength - bufsize)
+
+                ## check for INVALIDACCESS ##
+
+                ## check vor VARIABLEOVERFLOW ##
+
+                ## check for RBPOVERFLOW ##
+                # parse distance between buffer_address and rbp
+                buffer_rbp_distance = int(buffer_address[4:], 16)
+                print("Offset of the buffer_address", buffer_rbp_distance)
+
+                if buffer_rbp_distance < inputlength:
+                    # bufferoverflow can reach rbp
+                    vuln = jsonio.create_vulnerability("RBPOVERFLOW", f_n, 'fgets', buffer['name'], instruction['address'])
+                    jsonio.add_vulnerability(vuln)
+
+        else:
+            print('ERROR: value of buffer_address is', buffer_address)
+            return False
+    else:
+        print('ERROR: Buffer not loaded using lea')
+        return False
+
+
+
+
+
+
+
+def get_instruction(f_n, number):
+    return p_data[f_n]['instructions'][number - 3]
+
+
+def check_strncat(f_n, instruction):
+    print(instruction)
 
 
 def get_var(f_n, address):
@@ -32,43 +114,36 @@ def get_var(f_n, address):
     print("get_var ERROR: No such address {} in function {}".format(address, f_n))
 
 
+# basic, for now
+dangerous_functions = {'<gets@plt>': check_gets, '<strcpy@plt>': check_strcpy, '<strcat@plt>': check_strcat,
+                       '<fgets@plt>': check_fgets, '<strncpy@plt>': check_strncpy, '<strncat@plt>': check_strncat}
 
-
-
-#basic, for now
-dangerous_functions = {'<gets@plt>': check_gets, '<strcpy@plt>': check_strcpy, '<strcat@plt>': check_strcat, '<fgets@plt>': check_fgets, '<strncpy@plt>': check_strncpy, '<strncat@plt>': check_strncat}
 
 def check_buffer_vuln():
-
     for f_n in p_data.keys():
-        print(f_n)
         for instr in p_data[f_n]['instructions']:
             if instr['op'] == 'call':
                 if instr['args']['fnname'] in dangerous_functions:
                     dangerous_functions[instr['args']['fnname']](f_n, instr)
 
 
-
 def check_vuln():
     check_buffer_vuln()
 
-    #check_dangerous_func()
+    # check_dangerous_func()
 
 
 def main(name):
-
     global p_data
-    p_data = parser(name)
+    p_data = jsonio.parser(name)
 
-    check_vuln()    
+    check_vuln()
     '''if (not check_buffer_exists()):
         print("No buffers found in this file! :-)")
         return False'''
 
+
 if __name__ == "__main__":
     import sys
-    main("public_tests/test" + sys.argv[1] + ".json")
-    
-    
-    
-   
+
+    main(sys.argv[1])
