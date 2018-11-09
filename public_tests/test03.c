42fe128995ce7508da2d9829863da7e3ad54fdd1
Schippmunk <johannes.struve@gmx.de>
2018-11-09 14:28:10 +0000

    - added public tests

---
+++ public_tests/test03.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+  int control;
+  char buf[64];
+
+  control = 3;
+  fgets(buf, 77, stdin);
+
+}
---
+++ public_tests/test04.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+  int control;
+  char buf[64];
+
+  control = 4;
+  fgets(buf, 81, stdin);
+
+}
