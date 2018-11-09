6107835af2c66b2aa892157aa5a8857e56748505
RuiLoureiro <rui.loureiro@tecnico.ulisboa.pt>
2018-11-09 17:16:53 +0000

    check functions

--- vuln_checker.py
+++ vuln_checker.py
@@ -1,40 +1,66 @@
 from parser import *
 
+p_data = {}
+
+def check_gets(f_n, instruction):  
+    print(instruction)    
+
+def check_strcpy(f_n, instruction): 
+    print(instruction)
+
+def check_strcat(f_n, instruction): 
+    print(instruction)
+
+def check_fgets(f_n, instruction): 
+    print(f_n)
+    print(instruction)
+    #print(get_var(f_n, 28381))
+
+def check_strncpy(f_n, instruction): 
+    print(instruction)
+
+def check_strncat(f_n, instruction): 
+    print(instruction)
+
+
+
+
+def get_var(f_n, address):
+    for var in p_data[f_n]['variables']:
+        if var['address'] == address:
+            return var
+    print("get_var ERROR: No such address {} in function {}".format(address, f_n))
+
+
+
+
+
 #basic, for now
-dangerous_functions = ['<gets@plt>', '<strcpy@plt>', '<strcat@plt>', '<fgets@plt>', '<strncpy@plt>', '<strncat@plt>']
+dangerous_functions = {'<gets@plt>': check_gets, '<strcpy@plt>': check_strcpy, '<strcat@plt>': check_strcat, '<fgets@plt>': check_fgets, '<strncpy@plt>': check_strncpy, '<strncat@plt>': check_strncat}
 
+def check_buffer_vuln():
 
-def check_buffer_vuln(p_data):
-
-
-    func_names = p_data['func_vars'].keys() 
-    print(func_names)
-    func_instrs = p_data['func_instrs']
-    
-    dangerous_calls = []
-
-    for f_n in func_names:
+    for f_n in p_data.keys():
         print(f_n)
-        for instr in func_instrs[f_n]:
+        for instr in p_data[f_n]['instructions']:
             if instr['op'] == 'call':
                 if instr['args']['fnname'] in dangerous_functions:
-                    dangerous_calls.append(instr)
-
-    return dangerous_calls
+                    dangerous_functions[instr['args']['fnname']](f_n, instr)
 
 
-def check_vuln(p_data):
-    dangerous_calls = check_buffer_vuln(p_data)
 
-    print(dangerous_calls)
+def check_vuln():
+    check_buffer_vuln()
+
     #check_dangerous_func()
 
 
 def main(name):
 
+    global p_data
     p_data = parser(name)
 
-    check_vuln(p_data)    
+    check_vuln()    
     '''if (not check_buffer_exists()):
         print("No buffers found in this file! :-)")
         return False'''
