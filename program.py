9968b732eb78efd7d42c7430772c6703535102a5
Schippmunk <johannes.struve@gmx.de>
2018-11-16 22:26:57 +0000

    - lots of comments for program.py

--- program.py
+++ program.py
@@ -9,44 +9,34 @@ data = {}
 p = []
 # the states of p containing a call to a dangerous function
 dangerous_functions_occurring = []
-var = {}
+# the variables declared in json, indexed by offset from rbp
 variables = {}
-
-reg_match = {
-    'dword_address': {'m': re.compile('DWORD PTR \[(rbp|rip)[+-]0x[0-9a-f]+\]'),
-                      'c': lambda x: int(x[15:len(x) - 1], 16)},
-    'qword_address': {'m': re.compile('QWORD PTR \[(rbp|rip|rdx)[+-]0x[0-9a-f]+\]'),
-                      'c': lambda x: int(x[15:len(x) - 1], 16),
-                      'get_reg': lambda x: x[11:14],
-                      'get_sign': lambda x: x[14]},
-    'byte_address': {'m': re.compile('BYTE PTR \[(rbp|rip|rdx)[+-]0x[0-9a-f]+\]'),
-                     'c': lambda x: int(x[14:len(x) - 1], 16),
-                     'get_reg': lambda x: x[10:13],
-                     'get_sign': lambda x: x[13]},
-    'rbp_address': {'m': re.compile('\[rbp-0x[0-9a-f]+\]'), 'c': lambda x: int(x[5:len(x) - 1], 16)},
-    'rbp_address_trimmed': {'m': re.compile('rbp-0x[0-9a-f]+'), 'c': lambda x: int(x[4:], 16)},
-    'relative_rbp_trimmed': {'m': re.compile('rbp-0x[0-9a-f]+'), 'c': lambda x: int(x[4:], 16)},
-    'hex_num': {'m': re.compile('0x[0-9a-f]+'), 'c': lambda x: int(x, 16)}
-}
-
+# the dangerous fuctions we consider
 dangerous_functions = ['<gets@plt>', '<strcpy@plt>', '<strcat@plt>',
                        '<fgets@plt>', '<strncpy@plt>', '<strncat@plt>']
-
-registers = {'rax': '', 'rbx': '', 'rcx': '', 'rdx': '', 'rdi': '', 'rsi': '', 'r8': '', 'r9': '', 'r10': '',
-             'r11': '', 'r12': '', 'r13': '', 'r14': '', 'r15': '', 'rbp': '', 'rsp': 0, 'rip': ''}
+# the names of the registers used
 reg_names = ['rax', 'rbx', 'rcx', 'rdx', 'rdi', 'rsi', 'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15', 'rbp',
              'rsp', 'rip']
 
 
 class Register:
+    """Models a register
+
+    Keeps track of its name and value, particularly if only parts of it such es eax instead of rax are read
+    """
+
+    # the name of the register, one of reg_names
     name = ''
-    # value inserted at beginning
+    # the current value it holds
     val = None
     # value in lower half
     e_val = None
+    # whether it has a value in the lower half
     has_e_val = False
 
     def set_val(self, val: int, is_e_val: bool = False) -> None:
+        """Sets the value val, or just the lower have if is_e_val is true"""
+
         if is_e_val:
             self.e_val = val
             self.has_e_val = True
@@ -54,37 +44,59 @@ class Register:
             self.val = val
 
     def get_val(self, is_e_val: bool = False) -> int:
+        """Returns the value val or just the lower half of the register, if is_e_val is true"""
+
         if is_e_val:
             return self.e_val
         else:
             return self.val
 
     def __init__(self, name: str, val=None):
-        self.name = name
+        """Constructor, does not require a value to be assigned and detects if just the lower half is used"""
+
         if name[0] == 'e':
+            self.name = 'r' + name[1:]
             self.e_val = val
             self.has_e_val = True
         else:
+            self.name = name
             self.val = val
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Returns a string representation of the Register object, used by print"""
+
         if self.has_e_val:
             appendix = ' | ' + str(self.e_val)
         else:
             appendix = ''
-        return "Register " + self.name + ':' + str(self.val) + appendix
+        return "Reg " + self.name + ':' + str(self.val) + appendix
 
 
 class Segment:
+    """Models a Segment of memory in the stack."""
+
+    # the register the offset is relative to. For now this is always rbp (of main)
     relative_to = 'rbp'
+    # the amount of reserved bytes
     bytes = 0
+    # the value stored at that location
     val = 0
+    # the dictionary of the json of the variable of the program
     var = None
 
-    def get_val(self):
+    def get_val(self) -> int:
+        """Returns the value val"""
+
         return self.val
 
     def __init__(self, bytes, var: dict = None, val: int = 0):
+        """Constructor.
+
+        :param bytes: the size of the segment. Can be an integer or one of QWORD, DWORD, BYTE
+        :param var: pass json of variable if it exists
+        :param val: the initial value in that segment
+        """
+
         if isinstance(bytes, int):
             self.bytes = bytes
         elif bytes == 'QWORD':
@@ -99,7 +111,9 @@ class Segment:
         self.var = var
         self.val = val
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Returns a string representation of the Register object, used by print"""
+
         appendix = ''
         if self.val:
             appendix = " with value " + str(self.val)
@@ -109,18 +123,22 @@ class Segment:
 
 
 class Variable:
+    """Models a variable of the C program, contains additional information to the json object"""
+
+    # the attributes from the json object
     address = ''
     bytes = 0
-    bytes_filled = 0
     name = ''
-    rbp_distance = 0
     type = ''
+    # the amount of bytes user input possibly filled this with. Used for strcat analysis
+    bytes_filled = 0
+    # the distance of the value from the rbp, also its address/key in our stack model
+    rbp_distance = 0
 
     def fill(self, bytes: int) -> None:
-        self.bytes_filled = self.bytes_filled + bytes
+        """Adds :param bytes: to bytes_filledf"""
 
-    def __repr__(self):
-        return "Variable: " + self.name + " " + str(self.bytes) + " " + str(self.rbp_distance) + " " + self.type
+        self.bytes_filled = self.bytes_filled + bytes
 
     def __init__(self, json_data: dict):
         self.address = json_data['address']
@@ -132,11 +150,16 @@ class Variable:
         self.rbp_distance = - rbp_distance
         self.bytes_filled = 0
 
+    def __repr__(self):
+        """Returns a string representation of the Register object, used by print"""
+
+        return "Variable: " + self.name + " " + str(self.bytes) + " " + str(self.rbp_distance) + " " + self.type
+
 
 class State:
     """Describes the state of the program after execution of the instruction inst"""
 
-    # current name of the function
+    # name of the function the instruction is in
     f_n = ''
 
     # the instruction
@@ -145,10 +168,10 @@ class State:
     # if the instruction is a function call, this will be a list of the instruction of the functions called
     children = []
 
-    # the register values at this point of the execution
+    # the register values after inst is executed
     reg_vals = {}
 
-    # the stack at this point of the execution
+    # the stack after inst is executed
     stack = {}
 
     # if this is a call instruction, this is the name of the function that gets called
@@ -158,6 +181,7 @@ class State:
 
     def __repr__(self, indent: str = '') -> str:
         """Called by print, should print all info about the state"""
+
         s = indent + "Data of state. Current f_n: " + self.f_n
         s = s + "\n" + indent + "inst:"
         s = s + indent + pformat(self.inst)
@@ -173,39 +197,64 @@ class State:
         return s
 
     def get_seg(self, address, at_e_val: bool = False) -> Segment:
+        """Returns the memory segment at address.
+
+        Address is either an integer, and then this will look for address as a key in self.stack. If  address is a
+        register, it uses the value of that register as key
+        """
         if isinstance(address, int):
             return self.stack[address]
         elif at_e_val or isinstance(address, Register):
             return self.stack[address.get_val(at_e_val)]
 
     def add_seg(self, pos: int, seg: Segment) -> None:
+        """Overrides stack at pos with seg"""
+
         self.stack[pos] = seg
 
     def get_reg_val(self, reg: str) -> Register:
+        """Returns the register object at index reg"""
+
         if reg[0] == 'e':
             reg = 'r' + reg[1:]
         return self.reg_vals[reg]
 
     def get_address_type(self, address: str) -> dict:
+        """Analyzes an address given in an assembly instruction, as dest or source"""
+
         if address[0] == 'B':
+            # the address is of type BYTE PTR [rbp-0x...]
             block = {'type': 'mem', 'len': 'BYTE', 'relative': address[10:13], 'sign': address[13],
                      'val': int(address[14:len(address) - 1], 16)}
             return block
+
         elif address[0] in ['Q', 'D']:
+            # the address is of type DWORD PTR [rbp-0x...]
+            # or QWORD PTR [rbp-0x...]
             block = {'type': 'mem', 'len': address[:5], 'relative': address[11:14], 'sign': address[14],
                      'val': int(address[15:len(address) - 1], 16)}
+            # Ignore those big offsets of rip, they were meaningless in all of our tests and are probably where
+            # user input is initially stored in in the program
             if address[:17] == 'QWORD PTR [rip+0x':
                 block['ignore'] = True
             else:
                 block['ignore'] = False
             return block
+
         elif address[0] == '[':
+            # address is of type [rbp-0x...]
             return {'type': 'mem', 'relative': address[1:4], 'sign': address[4],
                     'val': int(address[5:len(address) - 1], 16)}
+
         elif address[0] == '0':
+            # the address is a hex number
+            # convert it to int
             val = int(address, 16)
             return {'type': 'num', 'val': val}
+
         else:
+            # the address is a register
+            # address2 is the actual register, if address is of the form e.g. eax
             if address[0] == 'e':
                 address2 = 'r' + address[1:]
                 is_e = True
@@ -214,85 +263,123 @@ class State:
                 is_e = False
 
             if address2 in self.reg_vals.keys():
+                # if there is already a register at address2 in this state return it
                 return {'type': 'reg', 'reg': self.reg_vals[address2], 'is_e': is_e}
             else:
+                # otherwise create a new Register instance and add it to reg_vals, return it
                 the_reg = Register(address2)
                 self.reg_vals[address2] = the_reg
                 return {'type': 'reg', 'reg': the_reg, 'is_e': is_e}
 
     def memory_op(self, inst: str, dest: dict, src: dict) -> None:
-        #print(dest)
-        #print(src)
-        #print('----- now analyzing')
+        """Handles the assembly instructions inst = mov, lea, sub, with dest and src being return values of
+        get_address_type
+        """
+
+        # print(dest)
+        # print(src)
+        # print('----- now analyzing')
         done = False
         if inst == 'sub':
+            # handle sub instruction
+
             if dest['type'] == 'reg':
+                # destination is a register
+
                 if src['type'] == 'reg':
+                    # source is a register, has not yet occurred
                     pass
+
                 elif src['type'] == 'num':
+                    # source is a hex number. convert it and update the destination register
                     val1 = self.get_reg_val(dest['reg'].name).get_val(dest['is_e'])
                     val2 = src['val']
                     dest['reg'].set_val(val1 - val2, dest['is_e'])
                     done = True
+
                 elif src['type'] == 'mem':
+                    # source is a memory address, has not yet occurred
                     pass
+
             elif dest['type'] == 'mem':
+                # destination is a memory addres, has not yet occurred
+
                 if src['type'] == 'reg':
                     pass
                 elif src['type'] == 'num':
                     pass
+
         elif inst == 'lea':
+            # load effective address, our address is the offset from rbp
             # src always memory, dest always register
             offset = src['val']
             self.reg_vals[dest['reg'].name].set_val(-offset)
             done = True
+
         elif inst == 'mov':
+            # move instruction
+
             if dest['type'] == 'reg':
+                # destination is a register
+
                 if src['type'] == 'reg':
+                    # source is a register
+                    # create a deep copy of the source register, important not to just use the same mutable object
                     the_copy = deepcopy(src['reg'])
                     the_copy.name = dest['reg'].name
                     self.reg_vals[dest['reg'].name] = the_copy
                     done = True
+
                 elif src['type'] == 'num':
+                    # source is a hex number, just update the value
                     self.reg_vals[dest['reg'].name].set_val(src['val'], dest['is_e'])
                     done = True
+
                 elif src['type'] == 'mem':
+                    # source is a memory address
+
                     if src['ignore']:
                         done = True
                     else:
+                        # one that is not on the list of ignored ones
+                        # read the value and put it into the register
                         seg = self.get_seg(-src['val'])
                         self.reg_vals[dest['reg'].name].set_val(seg.val, dest['is_e'])
                         done = True
 
             elif dest['type'] == 'mem':
+                # destination is a memory address
                 if src['type'] == 'reg':
+                    # source is a register, does not occur in basic tests
                     pass
                 elif src['type'] == 'num':
+                    # source is a hex number that will be used as a memory address
                     offset = dest['val']
                     if -offset in self.stack.keys():
                         existing_segment = self.get_seg(-offset)
                         existing_segment.val = src['val']
                         done = True
 
-        #print("Stack now")
-        #print(self.stack)
-        #print("Reg now")
-        #print(self.reg_vals)
+        # print("Stack now")
+        # print(self.stack)
+        # print("Reg now")
+        # print(self.reg_vals)
         if not done:
-            print("---------------------------")
-            print(inst)
-            print(dest)
-            print(src)
-            print(bcolors.FAIL)
-            print("INSTRUCTION NOT ANALYZED")
-            print(bcolors.ENDC)
+            pass
+            # print("---------------------------")
+            # print(inst)
+            # print(dest)
+            # print(src)
+            # print(bcolors.FAIL)
+            # print("INSTRUCTION NOT ANALYZED")
+            # print(bcolors.ENDC)
 
     def add_reg_val(self, inst: str, reg: str, val: str) -> None:
         """Adds to the registers of the current state the new value at register reg. How this is handled
         depends on the instruction inst, which is sub, mov or lea.
         """
-        #print('\n-----------------------', self.f_n)
-        #print(inst + " " + reg + " " + val)
+        # print('\n-----------------------', self.f_n)
+        # print(inst + " " + reg + " " + val)
 
         dest = self.get_address_type(reg)
         src = self.get_address_type(val)
@@ -427,6 +514,8 @@ def analyze_inst(inst: dict, f_n: str, append_to: list, prev_reg: list = []) -> 
 
 
 def add_variable_positions(stack: dict) -> None:
+    """Fills the global variable variables and sorts it"""
+
     global variables
     for f_n in data.keys():
         variables[f_n] = {}
@@ -434,16 +523,19 @@ def add_variable_positions(stack: dict) -> None:
             v = Variable(v)
             stack[v.rbp_distance] = Segment(v.bytes, v)
             variables[f_n][v.rbp_distance] = v
+    # TODO: See if this actually sorts by keys
     sorted(variables)
 
 
 def print_list():
     """utility method for printing the tree"""
+
     for s in p:
         print(s)
 
 
 def process_json(the_data):
+    """Starts the analysis. Sets global vars and calls analysis functions with initial values"""
     global data, var, p
     data = the_data
     # pprint(data)
